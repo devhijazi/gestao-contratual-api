4343edb66851ae7a8f5f00f8a905ea78d444ce84
AvengerSuicide <dev.avenger.suicide@gmail.com>
2020-03-02 17:38:25 -0300

    all

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 module.exports = {
-    //Loaders
-    Loaders: require('./loaders'),
+  //Loaders
+  Loaders: require("./loaders"),
 
-    //Banco de dados
-    mongoDB: require('./database/MongoDB.js'),
+  //Banco de dados
+  mongoDB: require("./database/MongoDB.js"),
 
-    //utils
-    ServerLogs: require('./utils/ServerLogs.js'),
+  //utils
+  ServerLogs: require("./utils/ServerLogs.js"),
 
-    //Estrututas ( structures)
-    Controller: require('./structures/Controller.js'),
-    Loader: require('./structures/Loader.js'),
+  //Estrututas ( structures)
+  Controller: require("./structures/Controller.js"),
+  Loader: require("./structures/Loader.js"),
 
-    //Rotas ( router )
-    Router: require('./router/Router.js'),
-    Controllers: Object.values(require('require-dir')('./router/controllers'))
-}
+  //Rotas ( router )
+  Router: require("./router/Router.js"),
+  Controllers: Object.values(require("require-dir")("./router/controllers"))
+};
